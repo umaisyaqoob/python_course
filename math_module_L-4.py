dd64a63d5a4bb8e4392203376cd023d4c5da4a8d
umaisyaqoob <umaisyaqoob333@gmail.com>
2023-11-16 16:56:09 +0500

    math module

---
+++ math_module_L-4.py
@@ -0,0 +1,40 @@
+import math
+
+
+#maximum
+list  = [1,2,3,4,5,6,7]
+maximum = max(list)
+print(maximum)
+
+#minimum
+list2  = [1,2,3,4,5,6,7]
+minimum = min(list2)
+print(minimum)
+
+#absolute
+num = -90
+absolute = abs(num)
+print(absolute)
+
+
+#power
+power = pow(3, 2)
+print(power)
+
+
+square_root = math.sqrt(64)
+print(square_root)
+
+ceil_num = 9.5
+celing = math.ceil(ceil_num)
+print(celing)
+
+
+floor_num = 9.5
+floor = math.floor(floor_num)
+print(floor)
+
+
+pie = math.pi
+print(pie)
+
