2ae7892de079e15855a8c51ce8ad00703b3afca1
umaisyaqoob <umaisyaqoob333@gmail.com>
2023-11-28 00:16:47 +0500

    abstract_classes_and_interfaces

---
+++ abstract_classes_and_interfaces_nov_27_2023.py
@@ -0,0 +1,22 @@
+"""Yahan Info class Person abstract class se inherit ho rahi hai.
+Ismein area method ko implement karna zaroori hai, warna aapko error milega."""
+
+from abc import ABC, abstractmethod
+
+class Person(ABC):
+
+    @abstractmethod
+    def personal(self):
+        pass
+
+class Info(Person):
+    def __init__(self, name):
+        self.__name = name
+
+    def personal(self):
+            return f'My name is {self.__name}'
+    
+
+info = Info("Muhammad Umais")
+
+print(info.personal())
